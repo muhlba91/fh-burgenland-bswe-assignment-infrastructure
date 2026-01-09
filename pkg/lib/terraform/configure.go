a22cf4f8edd936a0a1e82e928e18da2c9fd4a275
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-01-09 15:38:46 +0100

    feat: create github secret for terraform bucket

--- pkg/lib/terraform/configure.go
+++ pkg/lib/terraform/configure.go
@@ -6,14 +6,21 @@ import (
 	"github.com/muhlba91/fh-burgenland-bswe-assignment-infrastructure/pkg/lib/config"
 	"github.com/muhlba91/fh-burgenland-bswe-assignment-infrastructure/pkg/model/config/repository"
 	"github.com/muhlba91/pulumi-shared-library/pkg/lib/aws/s3/bucket"
+	"github.com/muhlba91/pulumi-shared-library/pkg/lib/github/actions/secret"
 	"github.com/muhlba91/pulumi-shared-library/pkg/util/defaults"
+	"github.com/pulumi/pulumi-github/sdk/v6/go/github"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 // Configure configures Terraform resources.
 // ctx: pulumi.Context.
 // repositories: list of repository configurations.
-func Configure(ctx *pulumi.Context, repositories []*repository.Config) (map[string]*pulumi.StringOutput, error) {
+// githubRepositories: list of created GitHub repositories.
+func Configure(
+	ctx *pulumi.Context,
+	repositories []*repository.Config,
+	githubRepositories map[string]*github.Repository,
+) (map[string]*pulumi.StringOutput, error) {
 	buckets := make(map[string]*pulumi.StringOutput)
 
 	for _, repo := range repositories {
@@ -30,6 +37,17 @@ func Configure(ctx *pulumi.Context, repositories []*repository.Config) (map[stri
 		if err != nil {
 			return nil, err
 		}
+
+		ghRepo, exists := githubRepositories[repo.Name]
+		if !exists {
+			return nil, fmt.Errorf("repository %s not found in created GitHub repositories", repo.Name)
+		}
+
+		secret.Write(ctx, &secret.WriteArgs{
+			Repository: ghRepo,
+			Key:        "TERRAFORM_BACKEND_BUCKET",
+			Value:      bucket.Bucket,
+		})
 
 		buckets[repo.Name] = &bucket.Bucket
 	}
