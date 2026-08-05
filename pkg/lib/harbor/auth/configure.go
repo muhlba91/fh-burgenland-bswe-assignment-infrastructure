7d3dc7f4cc41feb018c617860797986293fe73ca
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2026-08-05 09:28:41 +0200

    feat: add ws2026 base structure

--- pkg/lib/harbor/auth/configure.go
+++ pkg/lib/harbor/auth/configure.go
@@ -13,12 +13,13 @@ func Configure(ctx *pulumi.Context) error {
 	_, err := harbor.NewConfigAuth(ctx, "harbor-auth-dex", &harbor.ConfigAuthArgs{
 		AuthMode:         pulumi.String("oidc_auth"),
 		OidcAutoOnboard:  pulumi.Bool(true),
-		OidcClientId:     pulumi.String("harbor"),
-		OidcClientSecret: pulumi.String(os.Getenv("DEX_HARBOR_CLIENT_SECRET")),
+		OidcClientId:     pulumi.String("registry"),
+		OidcClientSecret: pulumi.String(os.Getenv("DEX_REGISTRY_CLIENT_SECRET")),
 		OidcEndpoint:     pulumi.String("https://auth.hochschule-burgenland.muehlbachler.xyz/api/dex"),
 		OidcName:         pulumi.String("GITHUB"),
 		OidcUserClaim:    pulumi.String("preferred_username"),
 		OidcGroupsClaim:  pulumi.String("groups"),
+		OidcAdminGroup:   pulumi.String("fhburgenland-bswe:dmuehlbachler"),
 		OidcScope:        pulumi.String("openid,email,profile,groups,offline_access"),
 	})
 	return err
